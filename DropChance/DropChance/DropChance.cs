76385ec51ce0766dccc85a8c4c079132bfee4f2d
Nikita <zazoozazoozaza@gmail.com>
2025-03-27 23:35:15 +0300

    DropChance<T> class
    DropChance<T> class with a simple test

---
+++ DropChance/DropChance/DropChance.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DropChance
+{
+    public static class DropChance<Key>
+    {
+        private static Random random = new Random();
+        public static Key GetRandomFromDictionary(Dictionary<Key, int> dictionary, int keySumm)
+        {
+            int randomNumber = random.Next(0, keySumm);
+            int elementSum = 0;
+            int index;
+
+            for (index = 0; index < dictionary.Count; index++)
+            {
+                elementSum += dictionary.ElementAt(index).Value;
+
+                if (elementSum > randomNumber)
+                {
+                    break;
+                }
+
+            }
+
+            return dictionary.ElementAt(index).Key;
+        }
+
+        public static int GetRangeOfRandom(Dictionary<Key, int> dictionary)
+        {
+            int range = 0;
+
+            for (int i = 0; i < dictionary.Count; i++)
+            {
+                if (dictionary.ElementAt(i).Value < 0)
+                {
+                    throw new ArgumentException("The chance of falling out cannot be a negative number", nameof(dictionary));
+                }
+
+
+                range += dictionary.ElementAt(i).Value;
+            }
+
+            return range;
+        }
+
+    }
+}
---
+++ DropChance/TestSuite/LibTests.cs
@@ -0,0 +1,22 @@
+﻿using DropChance;
+
+namespace TestSuite
+{
+    [TestClass]
+    public class LibTests
+    {
+        [TestMethod]
+        public void GetRandomFromDictionary_StandartDictionary_ReturnsRandomKey()
+        {
+            string firstKey = "";
+            Dictionary<string, int> firstDictionary = new Dictionary<string, int>()
+            {
+                { "First", 10 },
+                { "Second", 30 },
+                { "Third", 60 }
+            };
+
+            Assert.IsInstanceOfType(firstKey, DropChance<string>.GetRandomFromDictionary(firstDictionary, 100).GetType());
+        }
+    }
+}
